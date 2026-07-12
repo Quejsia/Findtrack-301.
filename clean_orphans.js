f812e5ef86e64c6d187875d7ebe2abade2edd387
Quejsia <165565729+Quejsia@users.noreply.github.com>
2026-07-12 22:17:57 +0800

    chore: remove unused firebase admin initialization

--- clean_orphans.js
+++
@@ -1,5 +0,0 @@
-const { initializeApp, cert } = require('firebase-admin/app');
-const { getFirestore } = require('firebase-admin/firestore');
-
-// Since we are in the environment, we can use application default credentials or we need the service account.
-// Wait, we can't easily run admin SDK without credentials.
